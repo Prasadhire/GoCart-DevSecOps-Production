a8c0270797ad969d84587578978fd51694d8fc0c
Prasad Hire <hirepraad002@gmail.com>
2026-06-16 17:58:14 +0000

    chnaging the instance type

--- devops/terraform/eks.tf
+++ devops/terraform/eks.tf
@@ -15,7 +15,7 @@ module "eks" {
   eks_managed_node_groups = {
     gocart_nodes = {
       name           = "gocart-node-group"
-      instance_types = ["t3.medium"]
+      instance_types = ["c7i-flex.large"]
 
       min_size     = 2
       max_size     = 5
